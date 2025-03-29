2bc410030c3d718cd3433ba12b7b0653f87a5ff3
Phel Viwath <phelviwathdj009@gmail.com>
2025-03-29 22:47:18 +0700

    Refactor: Implement blur effect for book cards based on view mode
    This commit refactors the `BookCardAdapter` to implement a blur effect on the book cards based on the selected view mode (classic or modern).  In modern mode, the card background is made transparent, and a blur effect is applied using `BlurView` from the 'BlurView' library. The blur intensity and color are adjusted dynamically based on the app's theme (light or dark). Additionally, a utility function `getTranslucentColor` is added to manage theme-specific translucent colors.

--- app/src/main/java/com/viwath/srulibrarymobile/presentation/view/adapter/BookCardAdapter.kt
+++ app/src/main/java/com/viwath/srulibrarymobile/presentation/view/adapter/BookCardAdapter.kt
@@ -7,14 +7,22 @@
 
 package com.viwath.srulibrarymobile.presentation.view.adapter
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
 import com.viwath.srulibrarymobile.databinding.ItemBookCardBinding
 import com.viwath.srulibrarymobile.domain.model.book.BookCard
+import com.viwath.srulibrarymobile.utils.applyBlur
+import com.viwath.srulibrarymobile.utils.getTranslucentColor
+import com.viwath.srulibrarymobile.utils.getTransparent
 
 class BookCardAdapter(
+    private val context: Activity,
     private val data: List<BookCard>,
+    private var isClassicMode: Boolean,
+    private var isDarkMode: Boolean
 ): RecyclerView.Adapter<BookCardAdapter.ViewHolder>(){
 
     inner class ViewHolder(val binding: ItemBookCardBinding): RecyclerView.ViewHolder(binding.root) {
@@ -24,6 +32,8 @@ class BookCardAdapter(
             binding.ivBook.setImageResource(item.icon)
             binding.tvValue.text = item.value.toString()
         }
+        val blurView = binding.blurViewBookInfo
+        val cardView = binding.cardBook
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -38,8 +48,15 @@ class BookCardAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
         holder.bind(item)
+        val color = "#80FFFFFF".toColorInt()
+        val translucentColor = context.getTranslucentColor(isDarkMode)
+
+        if (!isClassicMode){
+            holder.cardView.setCardBackgroundColor(context.getTransparent())
+            holder.blurView.applyBlur(context, 25f, translucentColor)
+        }
     }
 
     override fun getItemCount(): Int = data.size
-
+    fun getData(): List<BookCard> = data
 }
--- app/src/main/java/com/viwath/srulibrarymobile/presentation/view/fragment/BookFragment.kt
+++ app/src/main/java/com/viwath/srulibrarymobile/presentation/view/fragment/BookFragment.kt
@@ -7,8 +7,11 @@
 
 package com.viwath.srulibrarymobile.presentation.view.fragment
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -16,13 +19,15 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import com.viwath.srulibrarymobile.R
-import com.viwath.srulibrarymobile.common.Loading
 import com.viwath.srulibrarymobile.databinding.FragmentBookBinding
 import com.viwath.srulibrarymobile.domain.model.book.BookCard
 import com.viwath.srulibrarymobile.presentation.view.activities.MainActivity
 import com.viwath.srulibrarymobile.presentation.view.adapter.BookCardAdapter
 import com.viwath.srulibrarymobile.presentation.view.adapter.ViewPagerAdapter
 import com.viwath.srulibrarymobile.presentation.viewmodel.BookFragmentViewModel
+import com.viwath.srulibrarymobile.presentation.viewmodel.SettingViewModel
+import com.viwath.srulibrarymobile.presentation.viewmodel.SettingViewModel.Companion.CLASSIC
+import com.viwath.srulibrarymobile.presentation.viewmodel.SettingViewModel.Companion.MODERN
 import kotlinx.coroutines.launch
 
 /**
@@ -34,22 +39,34 @@ import kotlinx.coroutines.launch
  *
  * @property _binding The binding instance for the fragment's layout. Nullable before onViewCreated.
  * @property binding The non-null binding instance for the fragment's layout.
- * @property loading A Loading instance for showing/hiding loading indicators.
  * @property viewModel The shared ViewModel for managing book-related data.
  * @property mainActivity The parent MainActivity instance.
  */
-class BookFragment : Fragment(R.layout.fragment_book){
+class BookFragment : Fragment(){
 
     private var _binding: FragmentBookBinding?= null
     private val binding get() = _binding!!
-    private lateinit var loading: Loading
+
     private val viewModel: BookFragmentViewModel by activityViewModels()
+    private val settingViewModel by activityViewModels<SettingViewModel>()
+
     private lateinit var mainActivity: MainActivity
+    private var bookCardAdapter: BookCardAdapter? = null
+
+    private var isClassicMode = true
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBookBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentBookBinding.bind(view)
-        loading = Loading(requireActivity())
         mainActivity = (requireActivity() as MainActivity)
 
         val fragmentList = listOf(
@@ -65,14 +82,27 @@ class BookFragment : Fragment(R.layout.fragment_book){
             getString(R.string.backup)
         )
 
-//        val isDarkMode = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
-//        val cardList = listOf(binding.cardTotalBook, binding.cardDonateBook, binding.cardBorrow, binding.cardExp, binding.cardBorrowToday, binding.cardReturn)
+        val isDarkMode = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
 
         binding.viewPagerBook.adapter = ViewPagerAdapter(fragmentList, requireActivity())
         TabLayoutMediator(binding.tabLayout, binding.viewPagerBook){tab, position ->
             tab.text = tabTitle[position]
         }.attach()
         // bind data
+
+        settingViewModel.viewMode.observe(viewLifecycleOwner) { viewMode ->
+            isClassicMode = when(viewMode){
+                CLASSIC -> true
+                MODERN -> false
+                else -> true
+            }
+
+            bookCardAdapter?.let { adapter ->
+                updateAdapterBackground(adapter, isDarkMode)
+            }
+        }
+
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.state.collect{ state ->
                 if (state.isLoading){
@@ -89,7 +119,8 @@ class BookFragment : Fragment(R.layout.fragment_book){
                         BookCard(resources.getString(R.string.return_today), R.drawable.img_return_book, state.returnToday),
                     )
 
-                    val adapter = BookCardAdapter(cardList)
+                    val adapter = BookCardAdapter(requireActivity(), cardList, isClassicMode, isDarkMode)
+                    bookCardAdapter = adapter
                     binding.recyclerBookCard.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     binding.recyclerBookCard.adapter = adapter
                 }
@@ -101,6 +132,22 @@ class BookFragment : Fragment(R.layout.fragment_book){
         }
 
 
+    }
+
+    private fun updateAdapterBackground(adapter: BookCardAdapter, isDarkMode: Boolean) {
+        // Create a new adapter with updated settings and same data
+        val updatedAdapter = BookCardAdapter(
+            requireActivity(),
+            adapter.getData(), // You'll need to add this method to your adapter
+            isClassicMode,
+            isDarkMode
+        )
+
+        // Update the RecyclerView
+        binding.recyclerBookCard.adapter = updatedAdapter
+
+        // Update reference
+        bookCardAdapter = updatedAdapter
     }
 
 }
